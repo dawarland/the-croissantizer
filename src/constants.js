c7b4cae09a22772f86820da31b65a93eb85f37d9
Williams <dawarland@gmail.Com>
2022-05-09 13:16:29 -0400

    add env heroku

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const SLACK_OAUTH_TOKEN = 'xoxb-1208966967986-3487016300722-oskMcjq58GROrqfMkVS8t2Qx'
+export const SLACK_OAUTH_TOKEN = process.env.SLACK_OAUTH_TOKEN
 export const BOT_SPAM_CHANNEL = '#g_aléatoire' // this is the channel you want your bot online & spam to go
